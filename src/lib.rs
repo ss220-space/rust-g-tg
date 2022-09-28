45dc9baf3eb6c0a7c8ec5678b2236969067f88d8
Bizzonium <azizonkg@gmail.com>
2022-09-28 23:08:45 +0300

    Feat: text (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,8 @@ pub mod unzip;
 pub mod url;
 #[cfg(feature = "worleynoise")]
 pub mod worleynoise;
+#[cfg(feature = "text")]
+pub mod text;
 
 #[cfg(not(target_pointer_width = "32"))]
 compile_error!("rust-g must be compiled for a 32-bit target");
