9d01c5f48acd7f7b282d640e0bd304798d6e7ca9
Dimach <dimach2142@gmail.com>
2024-04-14 11:36:06 +0300

    Add x64 build

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,3 @@ pub mod unzip;
 pub mod url;
 #[cfg(feature = "worleynoise")]
 pub mod worleynoise;
-
-#[cfg(not(target_pointer_width = "32"))]
-compile_error!("rust-g must be compiled for a 32-bit target");
